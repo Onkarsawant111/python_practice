52a1c8907a9436b4f6dbcccbf8ae863c126a8125
onkar <onkarsawant111@gmail.com>
2024-05-25 22:18:25 +0530

    updated

--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
-    path('room/<str:pk>/',views.room) 
+    path('',views.home, name='home'),
+    path('room/<str:pk>/',views.room, name='room') 
 ]
 
 
--- base/views.py
+++ base/views.py
@@ -15,7 +15,7 @@ def room(request, pk):
     room = None
     for i in rooms:
         if i['id'] == int(pk):
-            room = i  
+            room = i
     context = {'room': room}
-    return render(request,'base/room.html',context)
+    return render(request,'base/room.html',context)  
 
